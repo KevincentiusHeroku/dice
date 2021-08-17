3c1b0e56068ca15c753b82b8e49d86a808b7fffa
kevincentius <kevincentius@yahoo.com>
2021-08-17 09:46:11 +0200

    before removing S snapshot tyep

--- src/piece-queue/piece-queue.spec.ts
+++ src/piece-queue/piece-queue.spec.ts
@@ -1,12 +1,19 @@
 import "reflect-metadata";
+import { mock } from "ts-mockito";
 import { container } from "tsyringe";
-import { RandomPieceGen } from "../piece-gen/random/random-piece-gen";
+import { PieceGen } from "../piece-gen/piece-gen";
 import { PieceQueue } from "./piece-queue";
 
 describe(PieceQueue.name, () => {
   it("should ", () => {
-
     const pieceQueue = container.resolve(PieceQueue);
 
+    const pieceGen = mock<PieceGen<any>>();
+
+    pieceQueue.init({
+      numHolds: 0,
+      numPreviews: 0,
+      pieceGen: pieceGen,
+    })
   });
 });
